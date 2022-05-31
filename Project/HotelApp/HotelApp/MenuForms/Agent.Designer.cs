07be76061c554e1f128f713dd165bd528aef27c4
srobichaud <srobichaud08@mynbcc.ca>
2022-05-31 18:56:16 -0300

    Created a few forms. In the process of creating the rest.

--- Project/HotelApp/HotelApp/MenuForms/Agent.Designer.cs
+++ Project/HotelApp/HotelApp/MenuForms/Agent.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.lblAgentProfile = new System.Windows.Forms.Label();
             this.lblCurrentAgent = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblAgentProfile
@@ -52,16 +53,28 @@
             this.lblCurrentAgent.TabIndex = 1;
             this.lblCurrentAgent.Text = "Currently viewing Agent\'s profile";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(152, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username:";
+            // 
             // Agent
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.lblCurrentAgent);
             this.Controls.Add(this.lblAgentProfile);
             this.Name = "Agent";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Agent";
+            this.Load += new System.EventHandler(this.Agent_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -71,5 +84,6 @@
 
         private System.Windows.Forms.Label lblAgentProfile;
         private System.Windows.Forms.Label lblCurrentAgent;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Project/HotelApp/HotelApp/MenuForms/Hotel.cs
+++ Project/HotelApp/HotelApp/MenuForms/Hotel.cs
@@ -16,5 +16,10 @@ namespace HotelApp.MenuForms
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
