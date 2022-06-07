08dbd503f9266777709072b0b266e87ef330d9d6
mpierce1988 <mpierce1988@gmail.com>
2022-06-06 22:23:03 -0300

    Finished First pass of Room dialog box. Still could use OK button to go back to Hotel screen

--- Project/HotelApp/HotelApp/MenuForms/Rooms.Designer.cs
+++ Project/HotelApp/HotelApp/MenuForms/Rooms.Designer.cs
@@ -34,11 +34,16 @@
             this.btnAdd = new System.Windows.Forms.Button();
             this.btnModify = new System.Windows.Forms.Button();
             this.btnConfirm = new System.Windows.Forms.Button();
+            this.cboRoomType = new System.Windows.Forms.ComboBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.lblSelectedRoomType = new System.Windows.Forms.Label();
+            this.btnCancelDelete = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvRooms)).BeginInit();
             this.SuspendLayout();
             // 
             // cboHotel
             // 
+            this.cboHotel.Enabled = false;
             this.cboHotel.FormattingEnabled = true;
             this.cboHotel.Location = new System.Drawing.Point(120, 43);
             this.cboHotel.Name = "cboHotel";
@@ -67,10 +72,11 @@
             this.dgvRooms.RowTemplate.Height = 24;
             this.dgvRooms.Size = new System.Drawing.Size(776, 127);
             this.dgvRooms.TabIndex = 2;
+            this.dgvRooms.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.HandleCellClick);
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(175, 356);
+            this.btnAdd.Location = new System.Drawing.Point(130, 356);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(100, 63);
             this.btnAdd.TabIndex = 3;
@@ -80,27 +86,69 @@
             // 
             // btnModify
             // 
-            this.btnModify.Location = new System.Drawing.Point(324, 356);
+            this.btnModify.Location = new System.Drawing.Point(277, 356);
             this.btnModify.Name = "btnModify";
             this.btnModify.Size = new System.Drawing.Size(100, 63);
             this.btnModify.TabIndex = 4;
             this.btnModify.Text = "Modify";
             this.btnModify.UseVisualStyleBackColor = true;
+            this.btnModify.Click += new System.EventHandler(this.btnModify_Click);
             // 
             // btnConfirm
             // 
-            this.btnConfirm.Location = new System.Drawing.Point(478, 356);
+            this.btnConfirm.Location = new System.Drawing.Point(424, 356);
             this.btnConfirm.Name = "btnConfirm";
             this.btnConfirm.Size = new System.Drawing.Size(100, 63);
             this.btnConfirm.TabIndex = 5;
             this.btnConfirm.Text = "Confirm";
             this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            // 
+            // cboRoomType
+            // 
+            this.cboRoomType.FormattingEnabled = true;
+            this.cboRoomType.Location = new System.Drawing.Point(303, 131);
+            this.cboRoomType.Name = "cboRoomType";
+            this.cboRoomType.Size = new System.Drawing.Size(169, 24);
+            this.cboRoomType.TabIndex = 6;
+            this.cboRoomType.SelectionChangeCommitted += new System.EventHandler(this.cboRoomType_SelectionChangeCommitted);
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(489, 131);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.ReadOnly = true;
+            this.txtDescription.Size = new System.Drawing.Size(217, 22);
+            this.txtDescription.TabIndex = 7;
+            // 
+            // lblSelectedRoomType
+            // 
+            this.lblSelectedRoomType.AutoSize = true;
+            this.lblSelectedRoomType.Location = new System.Drawing.Point(147, 134);
+            this.lblSelectedRoomType.Name = "lblSelectedRoomType";
+            this.lblSelectedRoomType.Size = new System.Drawing.Size(139, 16);
+            this.lblSelectedRoomType.TabIndex = 8;
+            this.lblSelectedRoomType.Text = "Selected Room Type:";
+            // 
+            // btnCancelDelete
+            // 
+            this.btnCancelDelete.Location = new System.Drawing.Point(571, 356);
+            this.btnCancelDelete.Name = "btnCancelDelete";
+            this.btnCancelDelete.Size = new System.Drawing.Size(100, 63);
+            this.btnCancelDelete.TabIndex = 9;
+            this.btnCancelDelete.Text = "Cancel";
+            this.btnCancelDelete.UseVisualStyleBackColor = true;
+            this.btnCancelDelete.Click += new System.EventHandler(this.btnCancelDelete_Click);
             // 
             // Rooms
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCancelDelete);
+            this.Controls.Add(this.lblSelectedRoomType);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.cboRoomType);
             this.Controls.Add(this.btnConfirm);
             this.Controls.Add(this.btnModify);
             this.Controls.Add(this.btnAdd);
@@ -124,5 +172,9 @@
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Button btnModify;
         private System.Windows.Forms.Button btnConfirm;
+        private System.Windows.Forms.ComboBox cboRoomType;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Label lblSelectedRoomType;
+        private System.Windows.Forms.Button btnCancelDelete;
     }
 }
--- Project/HotelApp/HotelApp/MenuForms/Rooms.cs
+++ Project/HotelApp/HotelApp/MenuForms/Rooms.cs
@@ -14,6 +14,8 @@ namespace HotelApp
     public partial class Rooms : Form
     {
         int? hotelID;
+        int lastSelectedRoomID;
+
         public Rooms()
         {
             InitializeComponent();
@@ -30,20 +32,16 @@ namespace HotelApp
         {
             try
             {
-                LoadHotelDropdown();
-
-                // if local hotelID is set, set to that hotel
-                if (hotelID.HasValue)
-                {
-                    cboHotel.SelectedValue = hotelID.Value;
-                    LoadRoomInformationIntoDGV((int)cboHotel.SelectedValue);
-                }
+                SetupBrowse();
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, ex.GetType().ToString());
             }
         }
+
+        
+
 
         private void cboHotel_SelectionChangeCommitted(object sender, EventArgs e)
         {
@@ -57,11 +55,156 @@ namespace HotelApp
             }
         }
 
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SetAddButtonState();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+            }
+        }
 
+        private void HandleCellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                // load values into combobox and text box up above
+
+                int? roomTypeID = dgvRooms.SelectedCells[0].OwningRow.Cells["RoomTypeID"].Value as int?;
+                string description = dgvRooms.SelectedCells[0].OwningRow.Cells["RoomTypeDescription"].Value.ToString();
+                int? roomID = dgvRooms.SelectedCells[0].OwningRow.Cells["RoomID"].Value as int?;
+
+                lastSelectedRoomID = roomID.Value;
+                cboRoomType.SelectedValue = roomTypeID;
+                txtDescription.Text = description;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+            }
+        }
+
+        private void cboRoomType_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            try
+            {
+                // check if blank row is selected. If so, return out of method
+                if (cboRoomType.SelectedValue == DBNull.Value)
+                {
+                    return;
+                }
+
+                // get room type description
+                int roomTypeID = Convert.ToInt32(cboRoomType.SelectedValue);
+
+                
+                
+
+                string sqlRoomDescription =
+                    $"SELECT RoomTypeDescription FROM RoomType WHERE RoomTypeID = {roomTypeID}";
+
+                string roomDescription = DataAccess.ExecuteScalar(sqlRoomDescription).ToString();
+
+                txtDescription.Text = roomDescription;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+            }
+        }
+
+        private void btnModify_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // set buttons to modify state
+                SetModifyButtonState();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+            }
+        }
+
+        private void btnCancelDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // determine if this is a Cancel or Delete
+
+                if (btnCancelDelete.Text.ToLower() == "cancel")
+                {
+                    // reset form to browse state
+                    SetupBrowse();
+                }
+                else if (btnCancelDelete.Text.ToLower() == "delete")
+                {
+                    DeleteRoom();
+                }
+                else
+                {
+                    throw new Exception("Cannot determine type of CancelDelete press");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+            }
+        }
+
+       
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // determine if this is a modify or an add
+                if (dgvRooms.SelectedCells.Count == 0)
+                {
+                    // this is an add
+                    AddNewRoom();
+                }
+                else
+                {
+                    // this is a modify
+                    ModifyRoomType();
+                }
+
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+            }
+        }
 
         #endregion
 
         #region Load Information
+
+        /// <summary>
+        /// Reloads values from the Database and sets the form
+        /// to Browse mode 
+        /// </summary>
+        private void SetupBrowse()
+        {
+            LoadHotelDropdown();
+
+            LoadRoomTypeDropdown();
+
+            // if local hotelID is set, set to that hotel
+            if (hotelID.HasValue)
+            {
+                cboHotel.SelectedValue = hotelID.Value;
+                LoadRoomInformationIntoDGV((int)cboHotel.SelectedValue);
+                cboHotel.Enabled = false;
+            }
+
+            SetBrowseButtonState();
+        }
 
         /// <summary>
         /// Loads all the related rooms for the given hotelID into the 
@@ -73,7 +216,7 @@ namespace HotelApp
             // create sql query that returns room information
             // for the given hotelID
             string sqlRoomQuery =
-                $@"SELECT RoomID, RoomTypeName, RoomTypeDescription 
+                $@"SELECT RoomID, Room.RoomTypeID, RoomTypeName, RoomTypeDescription 
 FROM Room INNER JOIN RoomType ON Room.RoomTypeID = RoomType.RoomTypeID
 WHERE HotelID = {hotelID}".Replace(Environment.NewLine, " ");
 
@@ -83,20 +226,7 @@ WHERE HotelID = {hotelID}".Replace(Environment.NewLine, " ");
             // assign result to dgv
             dgvRooms.DataSource = roomsResult;
 
-            // create new column for Room Type drop down box
-            DataGridViewColumn comboBoxColumn = new DataGridViewComboBoxColumn();
-            comboBoxColumn.Name = "RoomTypeColumn";
-            comboBoxColumn.Width = 100;
-            comboBoxColumn.HeaderText = "Room Type";
-            // add to dgv
-            dgvRooms.Columns.Add(comboBoxColumn);
-
-            // loop through rows in dgv and add room type combo box
-
-            foreach (DataGridViewRow item in collection)
-            {
-
-            }
+            
         }
 
         /// <summary>
@@ -115,33 +245,224 @@ WHERE HotelID = {hotelID}".Replace(Environment.NewLine, " ");
             UIUtilities.BindListControl(cboHotel, "HotelID", "HotelName", dt, true);
         }
 
+        /// <summary>
+        /// Load Room Type dropdown
+        /// </summary>       
+        private void LoadRoomTypeDropdown()
+        {
+            // create sql query to load room types into dropdown
+            string sqlRoomTypes =
+                "SELECT RoomTypeID, RoomTypeName FROM RoomType ORDER BY RoomTypeName ASC";
+
+            // save result to datatable
+            DataTable roomTypeResults = DataAccess.GetData(sqlRoomTypes);
+
+            // bind to dropdown with a blank
+            UIUtilities.BindListControl(cboRoomType, "RoomTypeID", "RoomTypeName", roomTypeResults, true);
+
+        }
+
+        /// <summary>
+        /// Sets the Enabled state for the CRUD buttons to Browsing mode
+        /// </summary>
+        private void SetBrowseButtonState()
+        {
+            // set button states for browsing
+            btnAdd.Enabled = true;
+            btnModify.Enabled = true;
+            btnConfirm.Enabled = false;
+            btnCancelDelete.Enabled = true;
+            btnCancelDelete.Text = "Delete";
+
+            dgvRooms.ReadOnly = false;
+            dgvRooms.Enabled = true;
+
+            cboHotel.Enabled = false;
+            cboRoomType.Enabled = false;
+
+            txtDescription.Text = "";
+        }
+
+        /// <summary>
+        /// Sets the Enabled state for the CRUD buttons to Add mode
+        /// </summary>
+        private void SetAddButtonState()
+        {
+            // set button states for browsing
+            btnAdd.Enabled = false;
+            btnModify.Enabled = false;
+            btnConfirm.Enabled = true;
+            btnCancelDelete.Enabled = true;
+            btnCancelDelete.Text = "Cancel";
+
+            dgvRooms.ReadOnly = true;
+            dgvRooms.ClearSelection();
+            dgvRooms.Enabled = false;
+
+            cboHotel.Enabled = false;
+            cboRoomType.Enabled = true;
+
+            cboRoomType.SelectedValue = DBNull.Value;
+        }
+
+        /// <summary>
+        /// Sets the Enabled state for the CRUD buttons to Add mode
+        /// </summary>
+        private void SetModifyButtonState()
+        {
+            // set button states for browsing
+            btnAdd.Enabled = false;
+            btnModify.Enabled = false;
+            btnConfirm.Enabled = true;
+            btnCancelDelete.Enabled = true;
+            btnCancelDelete.Text = "Cancel";
+
+            dgvRooms.ReadOnly = true;
+            // disable dgv so it cannot be selected again
+            dgvRooms.Enabled = false;
+            
+
+            cboHotel.Enabled = false;
+            cboRoomType.Enabled = true;
+        }
 
         #endregion
 
-        private void btnAdd_Click(object sender, EventArgs e)
+        #region Create, Update, and Delete Information
+
+        /// <summary>
+        /// Delete's the current room, as long as there are no current bookings for the room
+        /// </summary>
+        private void DeleteRoom()
         {
-            try
+            // if no row is selected, show message and return
+            if(dgvRooms.SelectedCells.Count == 0)
             {
-                // add row to dgv
-                dgvRooms.AllowUserToAddRows = true;
-                // make datagrid editable
-                dgvRooms.ReadOnly = false;
+                throw new Exception("Please select a room to delete");
+            }
 
-                // set all rows except last row to read only
-                foreach (DataGridViewRow record in dgvRooms.Rows)
-                {
-                    if(record.Cells["RoomID"].ToString() != "")
-                    {
-                        record.ReadOnly = true;
-                    }
-                }
-            }
-            catch (Exception ex)
+            // get room ID for selected room
+            int? roomID = dgvRooms.SelectedCells[0].OwningRow.Cells["RoomID"].Value as int?;
+
+            // null check
+            if (!roomID.HasValue)
             {
-                MessageBox.Show(ex.Message, ex.GetType().ToString());
+                throw new Exception("Unable to retrieve RoomID for the selected room");
             }
+
+            // check if any bookings have this room
+            // create sql query string to get num of bookings with this roomID
+            string sqlBookingsWithRoom =
+                $"SELECT COUNT(*) FROM Booking WHERE RoomID = {roomID.Value}";
+
+            // execute query and save result to an int
+            int? bookingsWithThisRoom = DataAccess.ExecuteScalar(sqlBookingsWithRoom) as int?;
+
+            // null check
+            if (!bookingsWithThisRoom.HasValue)
+            {
+                throw new Exception("Unable to find if this room has any associated bookings before deleting");
+            }
+
+            // tell user that if there are bookings attached to this room, remove them before deleting this room
+            if(bookingsWithThisRoom.Value > 0)
+            {
+                MessageBox.Show("Cannot delete this room. Please remove any bookings for this room and try again", "Access Denied", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            // create sql query to delete room
+            string sqlDeleteRoom =
+                $"DELETE FROM Room WHERE RoomID = {roomID}";
+
+            // execute command and save rows affected
+            int rowsDeleted = DataAccess.ExecuteNonQuery(sqlDeleteRoom);
+
+            // display message to user indicating status of delete
+            if(rowsDeleted == 0)
+            {
+                MessageBox.Show("Something went wrong, and the room was not deleted");
+            }
+            else
+            {
+                MessageBox.Show("Room " + roomID
+                    + " was deleted successfully!");
+            }
+
+            // go back to initial browse state
+            SetupBrowse();
         }
-    }
 
+        /// <summary>
+        /// Creates a new room from the Room type selected
+        /// </summary>
+        private void AddNewRoom()
+        {
+            // if no room type selected, return out of method
+            if(cboRoomType.SelectedValue == DBNull.Value)
+            {
+                MessageBox.Show("Please select a room type to create a new room");
+                return;
+            }
+
+            // get information into variables
+            // we already have the hotelID
+            int roomTypeID = Convert.ToInt32(cboRoomType.SelectedValue);
+
+            // create sql insert statement
+            string sqlInsertRoom =
+                $"INSERT INTO Room (HotelID, RoomTypeID) VALUES ({hotelID}, {roomTypeID})";
+
+            // execute and save rows affected
+            int? rowsAffected = DataAccess.ExecuteNonQuery(sqlInsertRoom);
+
+            if(rowsAffected.HasValue || rowsAffected.Value > 0)
+            {
+                MessageBox.Show("You have successfully created a new room!");
+            }
+
+            SetupBrowse();
+        }
+
+        private void ModifyRoomType()
+        {
+            // if no room type selected, show message and return
+            if(cboRoomType.SelectedValue == DBNull.Value)
+            {
+                MessageBox.Show("Please select a new room type");
+                return;
+            }
+
+            // create sql statement that will update the current room based
+            // on the roomID in lastSelectedRoomID
+
+            int selectedRoomType = Convert.ToInt32(cboRoomType.SelectedValue);
+
+            // get selected roomID
+            int? roomID = dgvRooms.SelectedCells[0].OwningRow.Cells["RoomID"].Value as int?;
+
+            // null check
+            if (!roomID.HasValue)
+            {
+                throw new Exception("Unable to find the RoomID for the desired modification.");
+            }
+
+            string sqlModifyRoom =
+                $"UPDATE Room SET RoomTypeID = {selectedRoomType} WHERE RoomID = {roomID.Value}";
+
+            // execute modify, and save rows affected to an int
+            int rowsAffected = DataAccess.ExecuteNonQuery(sqlModifyRoom);
+
+            // show message is update was successful
+            if(rowsAffected == 1)
+            {
+                MessageBox.Show("Room record successfully updated!");
+            }
+
+            SetupBrowse();
+        }
+
+        #endregion
+    }
 
 }
